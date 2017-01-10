871f880c57aeac2eac22f5cc7ac38d0d53a6b14f
Owen Nelson <onelson@gmail.com>
2017-01-09 23:44:42 -0800

    major, minor, patch

--- build.rs
+++ build.rs
@@ -2,5 +2,5 @@ extern crate pkg_config;
 
 fn main() {
     pkg_config::Config::new()
-        .atleast_version("2.0").statik(true).probe("flite").unwrap();
+        .atleast_version("2.0.0").statik(true).probe("flite").unwrap();
 }
