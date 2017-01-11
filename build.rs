80ab1000f4ed6a292123a854bbbe2c24c2e64a21
Owen Nelson <onelson@gmail.com>
2017-01-11 00:28:58 -0800

    adding flite tarball, unpack during build, compile, then link

--- build.rs
+++ build.rs
@@ -1,6 +1,17 @@
-extern crate pkg_config;
+use std::process::Command;
 
 fn main() {
-    pkg_config::Config::new()
-        .atleast_version("2.0.0").statik(true).probe("flite").unwrap();
+    let src_dir = "./flite-2.0.0-release";
+    let out_dir = "./flite-2.0.0-release/build/x86_64-linux-gnu/lib";
+
+    Command::new("tar").args(&["xf", "flite-2.0.0-release.tar.bz2"]).status().unwrap();
+    Command::new("sh").args(&["configure", "--with-pic"])
+        .current_dir(&src_dir)
+        .status().unwrap();
+    Command::new("make")
+        .current_dir(&src_dir)
+        .status().unwrap();
+
+    println!("cargo:rustc-link-search=native={}", out_dir);
+    println!("cargo:rustc-link-lib=static=flite");
 }
